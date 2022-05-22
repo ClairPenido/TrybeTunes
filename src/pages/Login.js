41a455962db96291e7d86c9ba99a560c0f1d34c2
Clair Penido <clairpenido@gmail.com>
2022-05-22 18:33:32 -0300

    requesito 2 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,50 @@
-import { React, Component } from 'react';
+import React from 'react';
 // import { Redirect } from 'react-router-dom';
 
-class Login extends Component {
+class Login extends React.Component {
+state = {
+  nameInput: '',
+  isSendButtonDisabled: true,
+};
+
+  onInputChange = (event) => {
+    this.setState({ nameInput: event.target.value }, () => this.verificaQuantNameInput());
+  }
+
+  verificaQuantNameInput = () => {
+    const { nameInput } = this.state;
+    const numCaracMin = 3;
+    if (nameInput.length >= numCaracMin) {
+      this.setState({ isSendButtonDisabled: false });
+    } else {
+      this.setState({ isSendButtonDisabled: true });
+    }
+  };
+
   render() {
+    const { nameInput, isSendButtonDisabled } = this.state;
     return (
       <div data-testid="page-login">
         <h2>Login</h2>
+        <form>
+          <label htmlFor="name-input">
+            Nome:
+            <input
+              value={ nameInput }
+              onChange={ this.onInputChange }
+              type="text"
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ isSendButtonDisabled }
+            data-testid="login-submit-button"
+          >
+            Entrar
+
+          </button>
+        </form>
       </div>
     );
   }
